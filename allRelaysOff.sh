d0ad2ab2771b30004dc57c2293150d1b7688031f
Mechputer <mlinguist@gmail.com>
2018-01-10 06:38:26 -0500

    SI7021/ 	auto.sh 	cabHumid.txt 	cabTemp.txt 	getTemp.sh 	relay10Off.sh 	relay10On.sh 	relay11Off.sh 	relay11On.sh 	relay12Off.sh 	relay12On.sh 	relay13Off.sh 	relay13On.sh 	relay14Off.sh 	relay14On.sh 	relay15Off.sh 	relay15On.sh 	relay16Off.sh 	relay16On.sh 	relay1Off.sh 	relay1On.sh 	relay2Off.sh 	relay2On.sh 	relay3Off.sh 	relay3On.sh 	relay4Off.sh 	relay4On.sh 	relay5Off.sh 	relay5On.sh 	relay6Off.sh 	relay6On.sh 	relay7Off.sh 	relay7On.sh 	relay8Off.sh 	relay8On.sh 	relay9Off.sh 	relay9On.sh 	temp.log 	tempHumid 	tempRange.txt 	tempSet.txt

---
+++ allRelaysOff.sh
@@ -0,0 +1,16 @@
+sudo /home/pi/scripts/relay1Off.sh
+sudo /home/pi/scripts/relay2Off.sh
+sudo /home/pi/scripts/relay3Off.sh
+sudo /home/pi/scripts/relay4Off.sh
+sudo /home/pi/scripts/relay5Off.sh
+sudo /home/pi/scripts/relay6Off.sh
+sudo /home/pi/scripts/relay7Off.sh
+sudo /home/pi/scripts/relay8Off.sh
+sudo /home/pi/scripts/relay9Off.sh
+sudo /home/pi/scripts/relay10Off.sh
+sudo /home/pi/scripts/relay11Off.sh
+sudo /home/pi/scripts/relay12Off.sh
+sudo /home/pi/scripts/relay13Off.sh
+sudo /home/pi/scripts/relay14Off.sh
+sudo /home/pi/scripts/relay15Off.sh
+sudo /home/pi/scripts/relay16Off.sh
